aaaf70eedac988d0bc2558cf8ba3291db07c2974
Jonathan Tobón <jtobonc@unicartagena.edu.co>
2025-07-03 03:26:29 -0500

    Cree los enums de acuerdo a mis clases

---
+++ src/main/java/com/udec/actividad4/dominio/enums/TipoActividad.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.udec.actividad4.dominio.enums;
+
+/**
+ *
+ * @author altoc
+ */
+public enum TipoActividad {
+    GRATUITA,
+    DE_PAGO
+}
---
+++ src/main/java/com/udec/actividad4/dominio/enums/TipoHabitacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.udec.actividad4.dominio.enums;
+
+/**
+ *
+ * @author altoc
+ */
+public enum TipoHabitacion {
+    INDIVIDUAL,
+    DOBLE,
+    TRIPLE,
+    SUITE
+}
