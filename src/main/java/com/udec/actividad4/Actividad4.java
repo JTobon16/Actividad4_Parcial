eae291b119c5840c3b34ca8ca30d045d3e72278e
Jonathan Tobón <jtobonc@unicartagena.edu.co>
2025-07-03 02:42:30 -0500

    Cree mi programa en NETBEANS

---
+++ src/main/java/com/udec/actividad4/Actividad4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.udec.actividad4;
+
+/**
+ *
+ * @author altoc
+ */
+public class Actividad4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
