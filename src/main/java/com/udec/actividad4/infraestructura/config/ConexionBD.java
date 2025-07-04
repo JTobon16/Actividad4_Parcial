e70c86ac7673ed0331c38c6b858b833c7b347b74
Jonathan Tobón <jtobonc@unicartagena.edu.co>
2025-07-03 23:01:11 -0500

    actualizo mi conexion ya que faltaba contraseña

--- src/main/java/com/udec/actividad4/infraestructura/config/ConexionBD.java
+++ src/main/java/com/udec/actividad4/infraestructura/config/ConexionBD.java
@@ -12,7 +12,7 @@ public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/Actividad4"; 
     private static final String USER = "root";
-    private static final String PASSWORD = ""; 
+    private static final String PASSWORD = "Tobonmysql"; 
 
     public static Connection obtenerConexion() {
         try {
